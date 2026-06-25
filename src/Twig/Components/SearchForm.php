ffb26d91c350485945a4a9deab4d153d399e3900
Symfinity <dev@symfinity.net>
2026-06-25 08:42:07 +0200

    Update

---
+++ src/Twig/Components/SearchForm.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfinity\UxBlocksExtended\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('SearchForm', template: '@UxBlocksExtended/components/SearchForm.html.twig')]
+final class SearchForm
+{
+}
